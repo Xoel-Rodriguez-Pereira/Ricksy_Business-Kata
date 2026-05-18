8d22d6deba319622b43aee3f9817ead739362d94
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-05-18 12:48:23 +0200

    feature: Añadir clase CreditCard e interfaz GuestDispatcher

---
+++ app/src/main/java/edu/teamrocket/ricksyBusiness/CreditCard.java
@@ -0,0 +1,33 @@
+package edu.teamrocket.ricksyBusiness;
+
+public class CreditCard {
+
+    private String owner;
+    private String number;
+    private double credit;
+    private final String SYMBOL;
+
+    CreditCard(String owner, String symbol) {
+        this.owner = owner;
+        this.SYMBOL = symbol;
+    }
+
+    public boolean pay(double cost) {
+        return credit >= cost ? true : false;
+    }
+
+    public String number() {
+        return this.number;
+    }
+
+    public double credit() {
+        return this.credit;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Owner: %s | Number: %s | Symbol: %s | Credit: %d
+                """.formatted(this.owner, this.number(), this.SYMBOL, this.credit());
+    }
+}   
---
+++ app/src/main/java/edu/teamrocket/ricksyBusiness/dispatcher/GuestDispatcher.java
@@ -0,0 +1,7 @@
+package edu.teamrocket.ricksyBusiness.dispatcher;
+
+import edu.teamrocket.ricksyBusiness.CreditCard;
+
+public interface GuestDispatcher {
+    public void dispatch(CreditCard creditCard);
+}
